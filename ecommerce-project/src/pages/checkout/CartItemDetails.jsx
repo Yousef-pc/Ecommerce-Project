4a1207fe1439d349ded14255b6e8a717f572ebef
YousefPc <syhosseini2021@gmail.com>
2026-08-13 13:16:25 +0330

    feat: Create update feature for the checkout page.

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,10 +1,26 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isQuantityBeingUpdated, setIsQuantityBeingUpdated] = useState(false);
+  const [inputValue, setInputValue] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
+  }
+
+  const updateCartItemQuantity = async () => {
+    setIsQuantityBeingUpdated(!isQuantityBeingUpdated);
+
+    if (isQuantityBeingUpdated === true) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(inputValue)
+      });
+
+      await loadCart();
+    }
   }
 
   return (
@@ -20,10 +36,31 @@ export function CartItemDetails({ cartItem, loadCart }) {
           {formatMoney(cartItem.product.priceCents)}
         </div>
         <div className="product-quantity">
-          <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+          <span className="quantity-section">
+            Quantity: 
+            <input 
+              className="quantity-input"
+              type="text"
+              style={{display: isQuantityBeingUpdated ? 'inline-block' : 'none'}}
+              value={inputValue}
+              onChange={(event) => {
+                setInputValue(event.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") updateCartItemQuantity();
+              }}
+            />
+            <span 
+              className="quantity-label"
+              style={{display: isQuantityBeingUpdated ? 'none' : 'inline-block'}}
+            >
+              {cartItem.quantity}
+            </span>
           </span>
-          <span className="update-quantity-link link-primary">
+          <span 
+            className="update-quantity-link link-primary"
+            onClick={updateCartItemQuantity}
+          >
             Update
           </span>
           <span 

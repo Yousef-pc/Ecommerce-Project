e6e5ab9531219c0255bf20cd78c411ae0bbc33a5
YousefPc <syhosseini2021@gmail.com>
2026-08-18 11:51:55 +0330

    fix: Handle negative numbers in formatMoney feat: Add more tests for formatMoney

--- ecommerce-project/src/utils/money.js
+++ ecommerce-project/src/utils/money.js
@@ -1,4 +1,5 @@
 // So this file contains utilities (or code) that helps us work with money.
 export function formatMoney(amountCents) {
-  return `$${(amountCents / 100).toFixed(2)}`;
+  const formatted = (Math.abs(amountCents) / 100).toFixed(2);
+  return amountCents < 0 ? `-$${formatted}` : `$${formatted}`;
 }

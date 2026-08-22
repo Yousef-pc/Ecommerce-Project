433c8b470e2530c6aae85cda64585628235e2948
YousefPc <syhosseini2021@gmail.com>
2026-08-22 12:51:06 +0330

    feat: save build result in ecommerce-backend

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -13,5 +13,8 @@ export default defineConfig({
         target: 'http://localhost:3000'
       }
     }
-  } // this is called a Server Proxy Configuration.
+  }, // this is called a Server Proxy Configuration.
+  build: {
+    outDir: '../ecommerce-backend/dist'
+  }
 })
